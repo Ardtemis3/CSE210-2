a5dc61133c6c0101d0ed53e9fb3ba06ab6988647
Ken Ard III <kennyard3@byui.edu>
2025-11-03 10:01:54 -0700

    class work

--- sandbox/Sandbox/Program.cs
+++ sandbox/Sandbox/Program.cs
@@ -2,8 +2,13 @@ using System;
 
 class Program
 {
+    static void TestValues(int x, float y, double z)
+    {
+        Console.WriteLine($"The values are: {x}, {y}, {z}.");
+    }
     static void Main(string[] args)
     {
+        // ----------------------------------------------------------------------------------------------------------------
         // Console.WriteLine("Hello Sandbox World!");
         // string firstName;
         // string lastName;
@@ -16,29 +21,45 @@ class Program
 
         // Console.WriteLine($"Your name is: {lastName}, {firstName} {lastName}.");
 
-        bool done = false;
+        // bool done = false;
+        // ----------------------------------------------------------------------------------------------------------------
+        // while (!done)
+        // {
+        //     Console.WriteLine("Input your age: ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age >= 0 && age <= 125)
+        //     {
+        //         done = true;
+        //         Console.WriteLine($"Your age: {age}");
+        //     }
+        // }
+        // done = false;
+        // // do while loops will always run atleast once
+        // do
+        // {
+        //     Console.WriteLine("Input your age: ");
+        //     int age = int.Parse(Console.ReadLine());
+        //     if (age >= 0 && age <= 125)
+        //     {
+        //         done = true;
+        //         Console.WriteLine($"Your age: {age}");
+        //     }
+        // } while (!done);
 
-        while (!done)
-        {
-            Console.WriteLine("Input your age: ");
-            int age = int.Parse(Console.ReadLine());
-            if (age >= 0 && age <= 125)
-            {
-                done = true;
-                Console.WriteLine($"Your age: {age}");
-            }
-        }
-        done = false;
-        // do while loops will always run atleast once
-        do
-        {
-            Console.WriteLine("Input your age: ");
-            int age = int.Parse(Console.ReadLine());
-            if (age >= 0 && age <= 125)
-            {
-                done = true;
-                Console.WriteLine($"Your age: {age}");
-            }
-        } while (!done);
+        // ----------------------------------------------------------------------------------------------------------------
+
+        // List<int> numbers = new List<int>();
+        // numbers.Add(10);
+        // numbers.Add(2234);
+        // numbers.Add(-234);
+        // numbers.Add(101);
+        // foreach (int n in numbers)
+        // {
+        //     Console.WriteLine($"The number is {n}.");
+        // }
+
+        // ----------------------------------------------------------------------------------------------------------------
+
+        // TestValues(10, (float)123.234, 789456132.4745123516114);
     }
 }
